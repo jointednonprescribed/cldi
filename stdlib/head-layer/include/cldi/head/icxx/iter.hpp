0e524c75b9b33b8773f1b33d58e51663d44c98dc
jointednonprescribed <jointednonprescribed@gmail.com>
2025-02-20 11:25:44 -0800

    Add proposed header (iter.h and icxx/iter.hpp) and source (c/iter.c and c++/iter.cxx) files for module 'iter'.

---
+++ stdlib/head-layer/include/cldi/head/icxx/iter.hpp
@@ -0,0 +1,15 @@
+
+#ifndef _cldi_head__ITER_H
+#define _cldi_head__ITER_H 1
+
+#ifndef _cldi_head__ITER_H
+#	include "../iter.h"
+#endif
+
+
+
+
+
+
+
+#endif // _cldi_head__ITER_H
---
+++ stdlib/head-layer/include/cldi/head/iter.h
@@ -0,0 +1,13 @@
+
+#ifndef _cldi_head__ITER_H
+#define _cldi_head__ITER_H 1
+
+#include "memory.h"
+
+
+
+
+
+
+
+#endif // _cldi_head__ITER_H
