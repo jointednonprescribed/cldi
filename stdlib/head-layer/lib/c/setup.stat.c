b438914de9f3b8c27fd4806a54e513392e1cbc6b
jointednonprescribed <jointednonprescribed@gmail.com>
2025-03-17 10:48:25 -0700

    Update head/setup/stat.h, head/setup/icxx/stat.hpp, and head/setup.stat.c to include prototypes and implementations for proper error catching and handling.

--- stdlib/head-layer/lib/c/setup.stat.c
+++ stdlib/head-layer/lib/c/setup.stat.c
@@ -6,8 +6,77 @@
 
 
 
-CLDISTAT CLDI_ERRNO = CLDI_SUCCESS;
+const char *const CLDI_NO_ERRNAME = "<anonymous>";
+const char *const CLDI_ERRNAME_WDEFRAG_RECOMMENDED = "WDEFRAG_RECOMMENDED";
+const char *const CLDI_ERRNAME_WLVL3 = "WLVL3";
+const char *const CLDI_ERRNAME_WLVL2 = "WLVL2";
+const char *const CLDI_ERRNAME_WLVL1 = "WLVL1";
+const char *const CLDI_ERRNAME_SUCCESS = "SUCCESS";
+const char *const CLDI_ERRNAME_WLVL0 = "WLVL0";
+const char *const CLDI_ERRNAME_TRUE = "TRUE";
+const char *const CLDI_ERRNAME_CMPE = "CMPE";
+const char *const CLDI_ERRNAME_FALSE = "FALSE";
+const char *const CLDI_ERRNAME_CMPL = "CMPL";
+const char *const CLDI_ERRNAME_CMPG = "CMPG";
+const char *const CLDI_ERRNAME_EUNKNOWN = "EUNKNOWN";
+const char *const CLDI_ERRNAME_ENO_IMPL = "ENO_IMPL";
+const char *const CLDI_ERRNAME_EOUT_OF_MEMORY = "EOUT_OF_MEMORY";
+const char *const CLDI_ERRNAME_EDEFRAG_REQUIRED = "EDEFRAG_REQUIRED";
+const char *const CLDI_ERRNAME_ETOO_FEW_ARGS = "ETOO_FEW_ARGS";
+const char *const CLDI_ERRNAME_EOVERFLOW = "EOVERFLOW";
+const char *const CLDI_ERRNAME_EOVERFLOW_ARG = "EOVERFLOW_ARG";
+const char *const CLDI_ERRNAME_EOVERFLOW_ATTR = "EOVERFLOW_ATTR";
+const char *const CLDI_ERRNAME_ENULL_ATTR = "ENULL_ATTR";
+const char *const CLDI_ERRNAME_ENULL_ARG = "ENULL_ARG";
+const char *const CLDI_ERRNAME_ENULL_SELF = "ENULL_SELF";
+const char *const CLDI_ERRNAME_EINVALID_ATTR = "EINVALID_ATTR";
+const char *const CLDI_ERRNAME_EINVALID_ARG = "EINVALID_ARG";
+const char *const CLDI_ERRNAME_EINVALID_SELF = "EINVALID_SELF";
+const char *const CLDI_ERRNAME_ENO_ACCESS = "ENO_ACCESS";
+const char *const CLDI_ERRNAME_ENOT_READABLE = "ENOT_READABLE";
+const char *const CLDI_ERRNAME_ENOT_WRITABLE = "ENOT_WRITABLE";
+const char *const CLDI_ERRNAME_ENOT_EXECUTABLE = "ENOT_EXECUTABLE";
+const char *const CLDI_ERRNAME_ENONEXISTENT = "ENONEXISTENT";
+const char *const CLDI_ERRNAME_EINCOMPATIBLE_TYPE = "EINCOMPATIBLE_TYPE";
+const char *const CLDI_NO_ERRDESC = "No description provided.";
+const char *const CLDI_ERRDESC_WDEFRAG_RECOMMENDED = "Heap defragmentation is recommended.";
+const char *const CLDI_ERRDESC_WLVL3 = "<Warning-Level-3>";
+const char *const CLDI_ERRDESC_WLVL2 = "<Warning-Level-2>";
+const char *const CLDI_ERRDESC_WLVL1 = "<Warning-Level-1>";
+const char *const CLDI_ERRDESC_SUCCESS = "No error occurred.";
+const char *const CLDI_ERRDESC_WLVL0 = "<Warning-Level-0>";
+const char *const CLDI_ERRDESC_TRUE = "Status evaluates to true.";
+const char *const CLDI_ERRDESC_CMPE = "Comparison evaluates terms as equal.";
+const char *const CLDI_ERRDESC_FALSE = "Status evaluates to false.";
+const char *const CLDI_ERRDESC_CMPL = "Comparison evaluates term as less.";
+const char *const CLDI_ERRDESC_CMPG = "Comparison evaluates term as greater.";
+const char *const CLDI_ERRDESC_EUNKNOWN = "Unknown error occurred.";
+const char *const CLDI_ERRDESC_ENO_IMPL = "No implementation provided for this operation.";
+const char *const CLDI_ERRDESC_EOUT_OF_MEMORY = "System or application ran out of usable memory.";
+const char *const CLDI_ERRDESC_EDEFRAG_REQUIRED = "Heap defragmentation is required.";
+const char *const CLDI_ERRDESC_ETOO_FEW_ARGS = "Not enough arguments were given for this operation";
+const char *const CLDI_ERRDESC_EOVERFLOW = "Operation produced an overflowing result (out of bounds or undefined).";
+const char *const CLDI_ERRDESC_EOVERFLOW_ARG = "Argument is an overflowed value (out of bounds or undefined).";
+const char *const CLDI_ERRDESC_EOVERFLOW_ATTR = "An attribute of self is an overflowed value (out of bounds or undefined).";
+const char *const CLDI_ERRDESC_ENULL_ATTR = "A required attribute of self is null.";
+const char *const CLDI_ERRDESC_ENULL_ARG = "A non-optional argument was given as null.";
+const char *const CLDI_ERRDESC_ENULL_SELF = "Self is constructed with null data, this operation cannot continue.";
+const char *const CLDI_ERRDESC_EINVALID_ATTR;
+const char *const CLDI_ERRDESC_EINVALID_ARG = "An argument was passed as an invalid value.";
+const char *const CLDI_ERRDESC_EINVALID_SELF = "Self is constructed with invalid data, this operation cannot continue.";
+const char *const CLDI_ERRDESC_ENO_ACCESS = "Access is denied for this operation.";
+const char *const CLDI_ERRDESC_ENOT_READABLE = "Data is not readable; Attempted read of non-readable data.";
+const char *const CLDI_ERRDESC_ENOT_WRITABLE = "Data is not writable; Attempted write on non-writable data. ";
+const char *const CLDI_ERRDESC_ENOT_EXECUTABLE = "Data is not executable; Attempted call on non-executable data.";
+const char *const CLDI_ERRDESC_ENONEXISTENT = "Attempted to reference a nonexistent entry.";
+const char *const CLDI_ERRDESC_EINCOMPATIBLE_TYPE = "Attempted conflation of incompatible or inconvertable data types.";
 
+cldiexc_t CLDI_ERROR = {
+	.exc_name = "SUCCESS",
+	.exc_desc = "No error occurred.",
+	.function = NULL,
+	.ec       = CLDI_SUCCESS
+};
 
 const char* cldiGetErrorName(CLDISTAT e)
 {
@@ -90,6 +159,192 @@ bool cldiStatPermissible(CLDISTAT e)
 bool cldiIsPermissible()
 {
 	return CLDI_STAT_ISPERMISSIBLE(CLDI_ERRNO);
+}
+
+cldiexc_t cldierrec(CLDISTAT __ec)
+{
+	return (cldiexc_t) {
+		.exc_name=cldiGetErrorName(__ec),
+		.exc_desc=CLDI_NO_ERRDESC,
+		.function=NULL,
+		.ec=__ec
+	};
+}
+cldiexc_t cldierr(CLDISTAT __ec, const char *desc)
+{
+	return (cldiexc_t) {
+		.exc_name=cldiGetErrorName(__ec),
+		.exc_desc=desc,
+		.function=NULL,
+		.ec=__ec
+	};
+}
+cldiexc_t cldinerr(CLDISTAT __ec, const char *name, const char *desc)
+{
+	return (cldiexc_t) {
+		.exc_name=name,
+		.exc_desc=desc,
+		.function=NULL,
+		.ec=__ec
+	};
+}
+cldiexc_t cldierrf(CLDISTAT __ec, void *func, const char *desc)
+{
+	return (cldiexc_t) {
+		.exc_name=cldiGetError(__ec),
+		.exc_desc=desc,
+		.function=func,
+		.ec=__ec
+	};
+}
+cldiexc_t cldinerrf(CLDISTAT __ec, void *func, const char *name, const char *desc)
+{
+	return (cldiexc_t) {
+		.exc_name=name,
+		.exc_desc=desc,
+		.function=func,
+		.ec=__ec
+	};
+}
+
+// Throws an exception without copying it to CLDI_ERROR, this is used internally,
+// and for this file only.
+void _cldithrowexc(cldiexc_t *exc)
+{
+	// THIS CODE IS DUE TO CHANGE ONCE THREAD-SAFE IO FUNCTIONS ARE ADDED
+
+	if (exc != NULL) {
+		const CLDISTAT ec = exc->ec;
+		if (!(CLDI_STAT_ISSUCCESS(ec))) {
+			const char *excname = (exc->exc_name == NULL)? cldiGetErrorName(ec) : exc->exc_name;
+			const char *excdesc = (exc->exc_desc == NULL)? cldiGetErrorDesc(ec) : exc->exc_desc;
+
+			if (exc->function != NULL)
+				fprintf(stderr, "Encountered an exception in function %p ", exc->function);
+			else
+				fprintf(stderr, "Encountered an exception ");
+			// continue error message with error name, desc, and code
+			fprintf(stderr, " with code %d (%s):\n\t%s\n", ec, excname, excdesc);
+		}
+	} else {
+		fprintf(stderr, "Attempt was made to throw nullptr exception...\n");
+	}
+}
+cldiexc_t* cldithrowec(CLDISTAT __ec)
+{
+	cldiexc_t e = cldierrec(__ec);
+
+	cldithrow(&e);
+
+	return &CLDI_ERROR;
+}
+cldiexc_t* cldithrowd(CLDISTAT __ec, const char *desc)
+{
+	cldiexc_t e = cldierr(__ec, desc);
+
+	cldithrow(&e);
+
+	return &CLDI_ERROR;
+}
+cldiexc_t* cldinthrow(CLDISTAT __ec, const char *name, const char *desc)
+{
+	cldiexc_t e = cldinerr(__ec, name, desc);
+
+	cldithrow(&e);
+
+	return &CLDI_ERROR;
+}
+cldiexc_t* cldithrowf(CLDISTAT __ec, void *func, const char *desc)
+{
+	cldiexc_t e = cldierrf(__ec, func, desc);
+
+	cldithrow(&e);
+
+	return &CLDI_ERROR;
+}
+cldiexc_t* cldinthrowf(CLDISTAT __ec, void *func, const char *name, const char *desc)
+{
+	cldiexc_t e = cldinerrf(__ec, func, name, desc);
+
+	cldithrow(&e);
+
+	return &CLDI_ERROR;
+}
+cldiexc_t* cldithrow(cldiexc_t *self)
+{
+	// set the current error to self
+	CLDI_ERROR.exc_name = self->exc_name;
+	CLDI_ERROR.exc_desc = self->exc_desc;
+	CLDI_ERROR.function = self->function;
+	CLDI_ERROR.ec       = self->ec;
+
+	_cldithrowexc(self);
+
+	return &CLDI_ERROR;
+}
+
+CLDISTAT    cldiExcGetErrno(cldiexc_t *self)
+{
+	if (self == NULL)
+		return CLDI_ENULL_ARG;
+	else
+		return self->ec;
+}
+void*       cldiExcGetFunction(cldiexc_t *self)
+{
+	if (self == NULL)
+		return CLDI_ENULL_ARG;
+	else
+		return self->function;
+}
+const char* cldiExcGetName(cldiexc_t *self)
+{
+	if (self == NULL)
+		return CLDI_ENULL_ARG;
+	else
+		return self->exc_name;
+}
+const char* cldiExcGetDesc(cldiexc_t *self)
+{
+	if (self == NULL)
+		return CLDI_ENULL_ARG;
+	else
+		return self->exc_desc;
+}
+bool        cldiExcSpecifiesFunction(cldiexc_t *self)
+{
+	if (self == NULL)
+		return false;
+	else
+		return self->function != NULL;
+}
+bool cldiExcIsWarning(cldiexc_t *self)
+{
+	if (self == NULL)
+		return false;
+	else
+		return CLDI_STAT_ISWARNING(self->ec);
+}
+bool cldiExcIsError(cldiexc_t *self)
+{
+	if (self == NULL)
+		return false;
+	else
+		return CLDI_STAT_ISERROR(self->ec);
+}
+bool cldiExcIsSuccess(cldiexc_t *self)
+{
+	if (self == NULL)
+		return false;
+	else
+		return CLDI_STAT_ISSUCCESS(self->ec);
+}
+bool cldiExcIsPermissible(cldiexc_t *self)
+{
+	if (self == NULL)
+		return false;
+	else
+		return CLDI_STAT_ISPERMISSIBLE(self->ec);
 }
 
 
