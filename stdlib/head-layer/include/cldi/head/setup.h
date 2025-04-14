429f49f4b46623f2e807beb3f575973792c86f6d
jointednonprescribed <jointednonprescribed@gmail.com>
2025-04-14 11:29:36 -0700

    Added a global variable ('CLDI_LIB_VER') for cldi library version, and cldi-head ('CLDI_HEAD_VER') version, so that all separate CLDI libraries will have remnants of their own version embedded after compilation.

--- stdlib/head-layer/include/cldi/head/setup.h
+++ stdlib/head-layer/include/cldi/head/setup.h
@@ -8,10 +8,13 @@
 
 /* CLDI Version */
 #define CLDI_VERSION 250000000UL
+const unsigned long CLDI_LIB_VER  = CLDI_VERSION;
+const unsigned long CLDI_HEAD_VER = CLDI_VERSION;
 #if CLDI_C_ONLY == false
 namespace cldi
 {
-	constexpr unsigned long VERSION = CLDI_VERSION;
+	constexpr unsigned long VERSION      = CLDI_VERSION;
+	constexpr unsigned long HEAD_VERSION = CLDI_VERSION;
 }
 #endif
 
